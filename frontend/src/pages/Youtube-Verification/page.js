5b25eb522a6a2df295809f636dcdd67e4cf7f9cf
GiwanthaAthukorala <giwanthaathu@gmail.com>
2026-08-12 12:17:35 +0530

    Youtube link update

--- frontend/src/pages/Youtube-Verification/page.js
+++ frontend/src/pages/Youtube-Verification/page.js
@@ -188,24 +188,25 @@ export default function YouTubeVerificationTask() {
           </div>
 
           {/* Admin Message */}
-          {/**  <div className="mb-8 bg-blue-50 p-4 rounded-lg">
+          <div className="mb-8 bg-blue-50 p-4 rounded-lg">
             <p className="text-gray-700 mb-3">
               Please visit this link and take a screenshot as proof:
             </p>
             <a
-              href=""
+              href="https://youtube.com/@i.de.afilms?si=zyOXDZ0NteiEuMpJ"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center justify-between p-4 bg-white rounded-lg hover:bg-blue-50 transition-colors duration-200 border border-gray-200 hover:border-blue-300 group"
             >
               <span className="text-gray-700 group-hover:text-blue-700">
-                Hotcooking girl - යූ ටියුබ් චැනල් එක සබ්ක්‍රයිබ් කරන්න (ඔයාලාගේ
-                youtube වල main account වලින් විතරක් subcribe කරන්න )
+                I.dE.Afilms - යූ ටියුබ් චැනල් එක සබ්ක්‍රයිබ් කරන්න (ඔයාලාගේ
+                youtube වල main account වලින් විතරක් subcribe කරන්න 100
+                Subscribe වෙනකම් 1 screenshot එකක් upload කරන්න)
               </span>
               <ExternalLink className="w-4 h-4 text-blue-600 group-hover:scale-110 transition-transform" />
             </a>
           </div>
-         <div className="mb-8 bg-blue-50 p-4 rounded-lg">
+          {/**  <div className="mb-8 bg-blue-50 p-4 rounded-lg">
             <p className="text-gray-700 mb-3">
               Please visit this link and take a screenshot as proof:
             </p>
